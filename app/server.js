3c21aa0e53b19099d30eb0cf6f29ca3e07c22150
Printo Varghese <printovarghese09@gmail.com>
2026-08-13 23:01:04 +0530

    Update application version

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello from Printo CI/CD Project!");
+    res.send("Hello from Printo CI/CD Project - Version 2!");
 });
 
 app.get("/health", (req, res) => {
